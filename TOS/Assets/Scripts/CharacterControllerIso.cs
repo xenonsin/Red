74c003e8f5a18b57e6678033e2402d19a27e27b5
xenonsin <kenoiwasp@gmail.com>
2014-05-11 07:57:56 -0700

    Beginning to Implement FSM for Sprites

--- TOS/Assets/Scripts/CharacterControllerIso.cs
+++ TOS/Assets/Scripts/CharacterControllerIso.cs
@@ -15,11 +15,12 @@ public class CharacterControllerIso : MonoBehaviour {
     private bool dashed;
     private float dashCoolDown = 2.0f;
 
-    private bool _grounded;
+    private SpriteManager _spriteManager;
 
 	// Use this for initialization
 	void Start () {
         _canMove = true;
+        _spriteManager = this.GetComponent<SpriteManager>();
 	
 	}
 	
@@ -36,19 +37,25 @@ public class CharacterControllerIso : MonoBehaviour {
 
         //Makes sure the player faces the direction they're moving.
        if (moveDirection != Vector3.zero)
-           transform.rotation = Quaternion.Slerp( transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime * roationSpeed);
-
+       {
+           _spriteManager.IsWalking = true;
+           transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime * roationSpeed);
+       }
+       else
+           _spriteManager.IsWalking = false;
+       Debug.Log(moveDirection);
         //try to move where mouse is facing?
        if(_canMove)
             rigidbody.MovePosition(rigidbody.position + moveDirection.normalized * speed * Time.deltaTime);
 
+        /*
        if (!dashed && Input.GetKey(KeyCode.Space))
        {
            dashed = true;
            rigidbody.AddForce(transform.forward * 50f, ForceMode.Impulse);
            StartCoroutine(CoolDown(() => dashed = false, dashCoolDown));
        }
-
+        */
       }
 
     public IEnumerator CoolDown(System.Action operation, float coolDown)
--- TOS/Assets/Scripts/SpriteManager.cs
+++ TOS/Assets/Scripts/SpriteManager.cs
@@ -2,21 +2,36 @@
 using System.Collections;
 
 
-public class SpriteManager : MonoBehaviour {
+public class SpriteManager : MonoBehaviour
+{
 
     private Animator _animator;
 
-    private int _currentDirection = 0;
+    private int _currentDirection;
 
-	// Use this for initialization
-	void Start () {
+    public bool IsWalking { get; set; }
+
+    // Use this for initialization
+    void Start()
+    {
         _animator = this.GetComponentInChildren<Animator>();
 
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentDirection();
+
+        if (IsWalking)
+            _animator.SetBool("Walking", true);
+        else
+            _animator.SetBool("Walking", false);
+    }
+
+    void GetCurrentDirection()
+    {
         var camPos = new Vector2(Camera.main.transform.forward.x, Camera.main.transform.forward.z);
         var parent = new Vector2(transform.forward.x, transform.forward.z);
         float enemyAngle = Vector2.Angle(camPos, parent);
@@ -25,16 +40,11 @@ public class SpriteManager : MonoBehaviour {
         if (cross.z > 0)
             enemyAngle = 360 - enemyAngle;
 
-       float arrayIndexFloat = Mathf.Round(enemyAngle / 360f * 8.0f) % 8.0f;
+        float arrayIndexFloat = Mathf.Round(enemyAngle / 360f * 8.0f) % 8.0f;
 
-       int arrayIndexInt = Mathf.FloorToInt(arrayIndexFloat);
-       if (_currentDirection != arrayIndexInt)
-       {
-           _currentDirection = arrayIndexInt;
+        _currentDirection = Mathf.FloorToInt(arrayIndexFloat);
 
-          _animator.SetInteger("Direction", arrayIndexInt);
-       }
+        _animator.SetInteger("Direction", _currentDirection);
         
-       Debug.Log("angle " + rigidbody.velocity);
-	}
+    }
 }
