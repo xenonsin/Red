f022ffdbad199d4bebbf73c3d11c5feb7017e543
xenonsin <kenoiwasp@gmail.com>
2014-05-14 22:00:31 -0700

    Added hit detection and knockback.

--- TOS/Assets/Scripts/PlayerAttackIso.cs
+++ TOS/Assets/Scripts/PlayerAttackIso.cs
@@ -4,11 +4,16 @@ using System.Collections;
 [RequireComponent(typeof(SpriteManager))]
 public class PlayerAttackIso : MonoBehaviour {
 
+    private float attackRadius = 5f;
+    private float attackKnockback = 10f;
+
     private float attackDistance = 5.0f;
     private float attackDelay = 0.5f;
+    private float attackAngle = 30f;
 
     private SpriteManager _spriteManager;
-    private CharacterControllerIso _characterController;
+    //private CharacterControllerIso _characterController;
+    private tk2dSpriteAnimator _animator;
 
     private Vector3 newPos;
 
@@ -16,7 +21,9 @@ public class PlayerAttackIso : MonoBehaviour {
 	void Start () {
 
         _spriteManager = this.GetComponent<SpriteManager>();
-        _characterController = this.GetComponent<CharacterControllerIso>();
+       // _characterController = this.GetComponent<CharacterControllerIso>();
+        _animator = this.GetComponentInChildren<tk2dSpriteAnimator>();
+        _animator.AnimationEventTriggered += AnimationEventHandler;
 	
 	}
 	
@@ -32,16 +39,9 @@ public class PlayerAttackIso : MonoBehaviour {
             _spriteManager.IsAttacking = true;
            // _characterController.CanMove = false;
         }
-        
-
-
-
-        
+                
         Debug.DrawRay(transform.position, transform.forward, Color.green);
         
-
-
-	
         //turn when mouse button is clicked
 
         //
@@ -60,4 +60,42 @@ public class PlayerAttackIso : MonoBehaviour {
         }
         transform.LookAt(newPos);
     }
+
+    void AnimationEventHandler(tk2dSpriteAnimator animator, tk2dSpriteAnimationClip clip, int frameNum)
+    {
+        //string str = animator.name + "\n" + clip.name + "\n" + "INFO: " + clip.GetFrame(frameNum).eventInfo;
+        string eventInfo = clip.GetFrame(frameNum).eventInfo;
+
+        if (eventInfo == "Attack Hit")
+            CheckRange();
+            //StartCoroutine(DealDamage());
+    }
+
+    void CheckRange()
+    {     
+        
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, attackRadius);
+
+        foreach (var hit in hitColliders)
+        {
+            if (hit && hit.tag == "Enemy")
+            {
+               var cone = Mathf.Cos(attackAngle * Mathf.Deg2Rad);
+                Vector3 dir = (hit.transform.position - transform.position).normalized;
+
+                if(Vector3.Dot(transform.forward, dir) > cone)
+                {
+                    //Target is within the cone.
+                    Debug.Log("Attack hit!");
+                    hit.rigidbody.AddForce(transform.forward * attackKnockback, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+    //Use the same vars you use to draw your Overlap SPhere to draw your Wire Sphere.
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+}
 }
